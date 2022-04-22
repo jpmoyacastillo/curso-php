840b7051a204f32212c99d893edf0bd439ee018e
JP <juanpablo@rindegastos.com>
2022-04-23 00:51:43 +0200

    First lines with PHP

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+// echo "Hello World! \n";
+
+$nombre = "Juan";
+$apellido = "Perez";
+
+// echo "Hola " . $nombre . " " . $apellido . "! \n"; 
+
+// echo "Hola $nombre $apellido! \n";
+
+echo "El resultado de 4 x 5 es: " . (4 * 5) . "\n";
