6de5e5bfb04dada04fd3e7c9608465064190e9b8
JP <juanpablo@rindegastos.com>
2022-04-26 00:53:50 +0200

    Variables y constantes

---
+++ sintaxis-basica.php
@@ -0,0 +1,31 @@
+<?php
+
+// echo "Hello World! \n";
+
+$nombre = "Juan";
+$apellido = "Perez";
+
+// echo "Hola " . $nombre . " " . $apellido . "! \n"; 
+
+// echo "Hola $nombre $apellido! \n";
+
+// echo "El resultado de 4 x 5 es: " . (4 * 5) . "\n";
+
+$personas = [
+    "Carlos" => 22,
+    "Pedro" => 15,
+    "Juan" => 65,
+];
+
+// var_dump($personas);
+// print_r($personas);
+
+// Esto es un comentario
+# Esto es un comentario
+
+/*
+Cualquier cosa,
+pero esto es un comentario
+*/
+
+echo "\n";
---
+++ variables.php
@@ -0,0 +1,11 @@
+<?php
+
+$numero_1 = 8;
+$numero_2 = 7;
+
+define('PI', 3.14);
+// define('PI', 4);
+
+echo PI;
+
+echo "\n";
